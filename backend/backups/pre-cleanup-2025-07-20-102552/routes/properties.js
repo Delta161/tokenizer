7796114e6f766a8adaed529ad2ab2a36025790c8
Delta161 <133611913+Delta161@users.noreply.github.com>
2025-07-20 21:00:51 +0200

    Delete backend/backups/pre-cleanup-2025-07-20-102552 directory

--- backend/backups/pre-cleanup-2025-07-20-102552/routes/properties.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createProperty } from '../controllers/propertyController.js';
-
-const router = express.Router();
-
-// POST /api/properties/create
-router.post('/create', createProperty);
-
-export default router;
